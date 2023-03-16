8790591eaeadbd793658bca79604eb1315a15399
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-03-16 22:51:20 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -23,10 +23,32 @@ class PlantsController < ApplicationController
     end
 
 
+    def destroy
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            plant.destroy
+            head :no_content
+        else 
+            render json: {error: "Plant not found"}
+        end
+    end
+
+    #update 
+    def update 
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            plant.update(plant_params)
+            render json: plant, status: :accepted
+        else
+            render json: {error: "Plant not found"}, status: :not_found
+        end
+    end
+
+
     private
 
     
     def plant_params
-        params.permit(:name, :species)
+        params.permit(:name, :image, :price)
     end
 end
